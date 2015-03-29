f81f9d9d43d5ec5aa74f2d1f271fd8be1ce05db6
ronen <narkisr@gmail.com>
2015-03-29 16:18:10 +0300

    using latest backup module conventions

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -1,18 +1,20 @@
-# backing up redis db to s3
-class celestial::backup {
+# Backing up redis db to s3
+class celestial::backup(
+  $key        = false,
+  $secret     = false,
+  $bucket     = false,
+  $passphrase = false,
+){
 
   backup::duply{'redis-backup':
-    source       => '/var/lib/redis/',
-    target       => hiera('s3::url'),
-    target_user  => hiera('s3::user'),
-    target_pass  => hiera('s3::pass'),
-    tmp          => '/tmp',
-    passphrase   => hiera('duply::passphrase')
+    source      => '/var/lib/redis/',
+    target      => $bucket,
+    target_user => $key,
+    target_pass => $secret,
+    passphrase  => $passphrase,
+    globs       => ''
   }
 
-  backup::schedule {'redis-backup':
-    hour        => '0',
-    type        => 'duply',
-  }
+  backup::duply::schedule {'redis-backup': }
 
 }
